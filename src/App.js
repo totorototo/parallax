fc8a8cc5ee8a99a97a0734087299a40eb1d02a9e
Toto RoToto <el.totorototo@gmail.com>
2019-11-07 22:03:07 +0100

    handle device orientation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSpring, animated } from "react-spring";
+import { withDeviceOrientation } from "react-fns";
 
 import "./App.css";
 
-const Xcoef = 0.1;
-const Ycoef = -0.1;
+const Xcoef = 0.5;
+const Ycoef = -0.5;
 
 const computeRatioX = distance => distance * 0.1;
 const computeRatioY = distance => distance * 0.2;
@@ -14,24 +15,25 @@ const transform = distance => (x, y) =>
     Ycoef *
     computeRatioY(distance)}px,0)`;
 
-const calc = (x, y) => [
-  x - window.innerWidth / 2,
-  y - window.innerHeight / 2
-];
+// const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
 
-function App() {
-  const [ props, set ] = useSpring(() => ({
-    xy: [ 0, 0 ],
+const App = ({ alpha, beta }) => {
+  const [props, set] = useSpring(() => ({
+    xy: [0, 0],
     config: { mass: 10, tension: 550, friction: 140 }
   }));
+
+  useEffect(() => {
+    set({ xy: [alpha, 90 - beta] });
+  }, [alpha, beta]);
 
   return (
     <div className="page">
       <div
         className="parallax"
-        onMouseMove={({ clientX: x, clientY: y }) => {
-          set({ xy: calc(x, y) });
-        }}
+        // onMouseMove={({ clientX: x, clientY: y }) => {
+        //   set({ xy: calc(x, y) });
+        // }}
       >
         {Array.from({ length: 10 }, (_, index) => (
           <animated.div
@@ -43,6 +45,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
-export default App;
+export default withDeviceOrientation(App);
