76255fea4f31e04f66f3ba8760abf6644163aeed
totorototo <el.totorototo@gmail.com>
2019-11-08 14:49:55 +0100

    add debug info

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { withDeviceOrientation } from "react-fns";
 
 import "./App.css";
 
-const Xcoef = 0.5;
-const Ycoef = -0.5;
+const Xcoef = 0.8;
+const Ycoef = -0.8;
 
 const computeRatioX = distance => distance * 0.1;
 const computeRatioY = distance => distance * 0.2;
@@ -16,17 +16,22 @@ const transform = distance => (x, y) =>
     computeRatioY(distance)}px,0)`;
 
 const App = ({ alpha, beta }) => {
-  const [props, set] = useSpring(() => ({
-    xy: [0, 0],
+  const [ props, set ] = useSpring(() => ({
+    xy: [ 0, 0 ],
     config: { mass: 10, tension: 550, friction: 140 }
   }));
 
-  useEffect(() => {
-    set({ xy: [alpha, 90 - beta] });
-  }, [alpha, beta]);
+  useEffect(
+    () => {
+      set({ xy: [ alpha, 90 - beta ] });
+    },
+    [ alpha, beta ]
+  );
 
   return (
     <div className="page">
+      <div>{`alpha: ${alpha}`}</div>
+      <div>{`beta: ${beta}`}</div>
       <div className="parallax">
         {Array.from({ length: 10 }, (_, index) => (
           <animated.div
